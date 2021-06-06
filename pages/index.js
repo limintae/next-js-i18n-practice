63445b0bdff6ad26d4b1da71e90f5e7c6b6cb8c1
limintae@gmail.com <limintae@gmail.com>
2021-06-06 21:15:36 +0900

    upload source

---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useTranslation } from "next-i18next";
+
+export const getStaticProps = async ({ locale }) => {
+  console.log("locale of getStaticProps", locale);
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+};
+
+export default function Home() {
+  const { t } = useTranslation("common");  
+
+  return (
+    <div>
+      <h1>NextJS i18n practice</h1>
+      <p>{t("header-message")}</p>
+      <div>
+        <Link href="/" locale="en">
+          <button>English</button>
+        </Link>
+        <Link href="/" locale="ko">
+          <button>한국어</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
